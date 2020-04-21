ec799e49cfdffa930d6792d96ce9a567eabf4d50
nadiaporton <nadia.porton@outlook.com>
2020-04-21 11:49:58 +0200

    next step

--- jquery 21 apr/jquery opstart CDN/js/myscript.js
+++ jquery 21 apr/jquery opstart CDN/js/myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     //schrijf hier pas je eigen code
-
+    //Basic syntax is: $(selector).action()
     //einde van je code
 });
 
---
+++ jquery 21 apr/jquery opstart lokaal/js/myscript.js
@@ -0,0 +1,10 @@
+//document ready function is to prevent any jQuery code from running before the document is finished loading (is ready).
+$(document).ready(function () {
+
+    //schrijf hier pas je eigen code
+    //Basic syntax is: $(selector).action()
+    //einde van je code
+});
+
+
+
