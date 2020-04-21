404b753fa7340f263eadc924b41f76e2ea642be6
nadiaporton <nadia.porton@outlook.com>
2020-04-21 13:00:50 +0200

    opdracht 2 gemaakt

--- jquery 21 apr/jquery opdracht 2/js/myscript.js
+++ jquery 21 apr/jquery opdracht 2/js/myscript.js
@@ -2,9 +2,12 @@
 $(document).ready(function () {
 
     //schrijf hier pas je eigen code
-
-
-
+    $('li').click(function () {
+        $(this).css('text-decoration', 'line-through');
+    });
+    $('li').dblclick(function () {
+        $(this).hide();
+    });
     //einde van je code
 });
 
